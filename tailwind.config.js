89b744b46b570ec341844f94026d789131af0573
Alvaro_Aquije <gonzalo_6824@outlook.com>
2021-10-27 13:00:56 -0500

    chore: :package: updated packages

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      // * Config for the custom font
+      fontFamily: {
+        inter: 'Inter',
+      },
+      colors: {
+        'ravn-black': '#121212',
+        'light-text': '#828282',
+        'darker-text': '#333333',
+        'ravn-error-red': '#EC5757',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+};
